bbe6187671cc199d7d286f734f078bb026c014fe
Debellatrix <vlatka.pavisic@gmail.com>
2014-05-10 17:38:47 +0200

    promjena login forme

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class CustomUser(models.Model):
     user = models.OneToOneField(User, primary_key=True)
