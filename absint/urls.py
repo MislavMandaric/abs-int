d23a8b6ee8aa97ba29d052f0a288c45f1c1d1e8e
Ines Jelovac <ines.jelovac@gmail.com>
2014-05-10 10:56:20 +0200

    početna stranica

--- absint/urls.py
+++ absint/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(app.urls)),
+    url(r'^', include('app.urls')),
 )
--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+import app
 
+from .views import Index
 urlpatterns = patterns('',
+	 url(r'^', Index.as_view(), name="index"),
 )
